9bbef63b25cc4a79d2a934230479950fbee83e91
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-07-28 15:32:05 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -1,7 +1,22 @@
 console.log('fib.js loaded');
 
 (function(){ // wrapping everything within an annonymous function
-    var h = '<h3 style="color:navy">Fibonacci generator</h3>'
+var f= '<form name="frm"> '
+f +='<table style="border:solid green 1px">'
+f +='<tr><td>Enter the degree</td>'
+f +='<td><input type=text name="txt" size=12"></td>'
+f +='<td><select name="myoption">'
+f +='<option name="Faren" value="Fahrenheit">F</option>'
+f +='<option name="Cels" value="Celsius">C</option>'
+f +='</select></td>'
+f +='<td><input type="button" value="Calculate" onclick="apply()"></td></tr>'
+f +='<tr><td>Equal Value</td>'
+f +='<td><input type=text name="txt1" size="12" READONLY></td><td></td>'
+f +='<td align=right>&copy<a href="https://www.hscripts.com" style="color:#3D366F;text-decoration:none;cursor:pointer;font-size:13px">hscripts.com</a></td></tr>'
+f +='</table>'
+f +='</form>'
+
+    var h = '<h3 style="color:navy">Temp Caclulator</h3>'
     h +='<button id="fibButton" type="button" class="btn btn-primary">Generate</button> '
     h +='Lenght: <input id="fibLength" size=4 value="20"> '
     h +='Seed: <input id="fibSeed" size=5 value="0,1"> '
@@ -20,6 +35,17 @@ console.log('fib.js loaded');
     fibLength.style.border=0
     // fib generator
     var fib = function(n,x){
+      var a=frm.myoption.value;
+      if(a=="Fahrenheit")
+      {
+      var val=frm.txt.value;
+      var tf=parseInt(val);
+      var tc=(5/9)*(tf-32);
+      var res=Math.round(tc*Math.pow(10,2))/Math.pow(10,2);
+      frm.txt1.value=res+" C";
+
+
+
         if(!n){n=10}
         if(!x){x= [0,1]}
         x.push(parseInt(x.slice(-1))+parseInt(x.slice(-2,-1)))
@@ -46,4 +72,3 @@ console.log('fib.js loaded');
     }
     //fibSeed.onblur=fibLength.onblur=function(){fibButton.click()}
 })()
-
