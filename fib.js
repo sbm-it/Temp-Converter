29a58f3e119d9581a820285a657f5921020c6488
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-07-30 12:03:49 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -29,46 +29,9 @@ sbmApps.render(f)
         //document.body.appendChild(dv)
   ///  }
     // style
-    fibSeed.style.color="blue"
-    fibSeed.style.border=0
-    fibLength.style.color="blue"
-    fibLength.style.border=0
-    // fib generator
-    var fib = function(n,x){
-      var a=frm.myoption.value;
-      if(a=="Fahrenheit")
-      {
-      var val=frm.txt.value;
-      var tf=parseInt(val);
-      var tc=(5/9)*(tf-32);
-      var res=Math.round(tc*Math.pow(10,2))/Math.pow(10,2);
-      frm.txt1.value=res+" C";
 
-
-
-        if(!n){n=10}
-        if(!x){x= [0,1]}
-        x.push(parseInt(x.slice(-1))+parseInt(x.slice(-2,-1)))
-        if(x.length>n){
-            return x
-        }else{
-            return fib(n,x)
-        }
     }
     // generate
-    fibButton.onclick=function(){
-        var x = fib(parseInt(fibLength.value),JSON.parse('['+fibSeed.value+']'))
-        fibArray.textContent=x.join(', ')
-        fibSeed.value=x.slice(-2) // reseed
-        if(x.slice(-1)[0]>100000000000000000000){
-            fibSeed.value='0,1' // if numbers are too big then reset seed
-        }
-        fibSeed.size=fibSeed.value.length+5 // adjust size of seed input element
-    }
-    fibSeed.onkeyup=fibLength.onkeyup=function(evt){
-        if(evt.keyCode==13){
-            fibButton.click()
-        }
-    }
+  
     //fibSeed.onblur=fibLength.onblur=function(){fibButton.click()}
 })()
