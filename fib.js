44129d318c9a2fd9cd2dec735c4478768d3f128a
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-07-30 10:50:27 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -1,8 +1,8 @@
 console.log('fib.js loaded');
 
 (function(){ // wrapping everything within an annonymous function
-var f= '<form name="frm"> '
-f +='<table style="border:solid green 1px">'
+//var f= '<form name="frm"> '
+var f ='<table style="border:solid green 1px">'
 f +='<tr><td>Enter the degree</td>'
 f +='<td><input type=text name="txt" size=12"></td>'
 f +='<td><select name="myoption">'
@@ -14,7 +14,7 @@ f +='<tr><td>Equal Value</td>'
 f +='<td><input type=text name="txt1" size="12" READONLY></td><td></td>'
 f +='<td align=right>&copy<a href="https://www.hscripts.com" style="color:#3D366F;text-decoration:none;cursor:pointer;font-size:13px">hscripts.com</a></td></tr>'
 f +='</table>'
-f +='</form>'
+//f +='</form>'
 
   //var h = '<h3 style="color:navy">Temp Caclulator</h3>'
   //  h +='<button id="fibButton" type="button" class="btn btn-primary">Generate</button> '
@@ -25,7 +25,7 @@ f +='</form>'
         sbmApps.render(f)
     }else{ // if fib.js is being called without any help
         var dv = document.createElement('div')
-        dv.innerHTML = f
+        dv.innerHTML = h
         document.body.appendChild(dv)
     }
     // style
