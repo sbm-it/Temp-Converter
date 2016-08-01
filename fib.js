a3274ebaab24e0ae2f4d1b7a74d37e28c93877af
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-08-01 08:52:15 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -15,10 +15,13 @@ f +='<td><input type=text name="txt1" size="12" READONLY></td><td></td>'
 f +='<td align=right>&copy<a href="https://www.hscripts.com" style="color:#3D366F;text-decoration:none;cursor:pointer;font-size:13px">hscripts.com</a></td></tr>'
 f +='</table>'
 //f +='</form>'
-  var dv = document.createElement('div')
-  dv.innerHTML = f
-  document.body.appendChild(dv)
-sbmApps.render(f)
+if(typeof(sbmApps)=='function'){ // if sbmApps library was loaded
+    sbmApps.render(f)
+}else{ // if fib.js is being called without any help
+    var dv = document.createElement('div')
+    dv.innerHTML = f
+    document.body.appendChild(dv)
+}
   //var h = '<h3 style="color:navy">Temp Caclulator</h3>'
   //  h +='<button id="fibButton" type="button" class="btn btn-primary">Generate</button> '
   //  h +='Lenght: <input id="fibLength" size=4 value="20"> '
