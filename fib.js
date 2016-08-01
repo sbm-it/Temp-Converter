14b7e616e9dbafb92893d920028bd007465bd8b9
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-08-01 09:24:00 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -19,30 +19,7 @@ console.log('fib.js loaded');
     fibLength.style.color="blue"
     fibLength.style.border=0
     // fib generator
-    var fib = function(n,x){
-        if(!n){n=10}
-        if(!x){x= [0,1]}
-        x.push(parseInt(x.slice(-1))+parseInt(x.slice(-2,-1)))
-        if(x.length>n){
-            return x
-        }else{
-            return fib(n,x)
-        }
-    }
-    // generate
-    fibButton.onclick=function(){
-        var x = fib(parseInt(fibLength.value),JSON.parse('['+fibSeed.value+']'))
-        fibArray.textContent=x.join(', ')
-        fibSeed.value=x.slice(-2) // reseed
-        if(x.slice(-1)[0]>100000000000000000000){
-            fibSeed.value='0,1' // if numbers are too big then reset seed
-        }
-        fibSeed.size=fibSeed.value.length+5 // adjust size of seed input element
-    }
-    fibSeed.onkeyup=fibLength.onkeyup=function(evt){
-        if(evt.keyCode==13){
-            fibButton.click()
-        }
-    }
+
+    
     //fibSeed.onblur=fibLength.onblur=function(){fibButton.click()}
 })()
