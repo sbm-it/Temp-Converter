2c893e2caee58d3845256210d56a751cfb6fc0be
rjpiazza <richard.piazza@stonybrookmedicine.edu>
2016-08-01 09:03:01 -0400

    Add files via upload

--- fib.js
+++ fib.js
@@ -2,24 +2,24 @@ console.log('fib.js loaded');
 
 (function(){ // wrapping everything within an annonymous function
 //var f= '<form name="frm"> '
-var f ='<table style="border:solid green 1px">'
-f +='<tr><td>Enter the degree</td>'
-f +='<td><input type=text name="txt" size=12"></td>'
-f +='<td><select name="myoption">'
-f +='<option name="Faren" value="Fahrenheit">F</option>'
-f +='<option name="Cels" value="Celsius">C</option>'
-f +='</select></td>'
-f +='<td><input type="button" value="Calculate" onclick="apply()"></td></tr>'
-f +='<tr><td>Equal Value</td>'
-f +='<td><input type=text name="txt1" size="12" READONLY></td><td></td>'
-f +='<td align=right>&copy<a href="https://www.hscripts.com" style="color:#3D366F;text-decoration:none;cursor:pointer;font-size:13px">hscripts.com</a></td></tr>'
-f +='</table>'
+//f +='<tr><td>Enter the degree</td>'
+  var h = '<h3 style="color:navy">Fibonacci generator</h3>'
+h +='<td><input type=text name="txt" size=12"></td>'
+//h +='<td><select name="myoption">'
+//h +='<option name="Faren" value="Fahrenheit">F</option>'
+//h +='<option name="Cels" value="Celsius">C</option>'
+//h +='</select></td>'
+h +='<td><input type="button" value="Calculate" onclick="apply()"></td></tr>'
+h +='<tr><td>Equal Value</td>'
+h +='<td><input type=text name="txt1" size="12" READONLY></td><td></td>'
+h +='<td align=right>&copy<a href="https://www.hscripts.com" style="color:#3D366F;text-decoration:none;cursor:pointer;font-size:13px">hscripts.com</a></td></tr>'
+
 //f +='</form>'
 if(typeof(sbmApps)=='function'){ // if sbmApps library was loaded
-    sbmApps.render(f)
+    sbmApps.render(h)
 }else{ // if fib.js is being called without any help
     var dv = document.createElement('div')
-    dv.innerHTML = f
+    dv.innerHTML = h
     document.body.appendChild(dv)
 }
   //var h = '<h3 style="color:navy">Temp Caclulator</h3>'
